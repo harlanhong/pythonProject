a235f9688e5026bf7a8bcb37eef146349a50d1ee
Administrator <758558048@qq.com>
2017-10-15 21:53:01 +0800

    17.10.15 21:52

--- faceFeature/pythonForTest.py
+++ faceFeature/pythonForTest.py
@@ -1,8 +1,10 @@
 import cv2
 
 
-img = cv2.imread("img/1.jpg",1)
-cv2.imshow("=img",img)
-ret1, th1 = cv2.threshold(img, (10,10,10), (255,255,255), cv2.THRESH_BINARY_INV)
-cv2.imshow("th",th1)
+temp = []
+temp.append((1,2))
+temp.append((3,4))
+print(temp[0])
+temp.remove((1,2))
+print(temp)
 cv2.waitKey(0)
