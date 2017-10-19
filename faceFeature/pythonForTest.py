518b044287171eb38a771bbeaf920decee331c93
harlanhong <758558048@qq.com>
2017-10-19 15:51:30 +0800

    17.10.19 15:51

--- faceFeature/pythonForTest.py
+++ faceFeature/pythonForTest.py
@@ -1,56 +1,13 @@
 import cv2
 import math
 import numpy as np
-
-def delete_jut(src,uthreshold,vthreshold,type):
-    threshold = 0
-    dst = src.copy()
-    sp = src.shape
-    height = sp[0]
-    width = sp[1]
-    k = 0
-    mode = (1-type)*255
-    modeInver = 255-mode
-    cont = 1;
-    for i in range(height-1):
-        for j in range(width-1):
-            cont +=1
-            #行消除
-            if dst[i,j] == mode and dst[i,j+1] == modeInver:
-                print(i, j,cont)
-                if j+uthreshold >= width:
-                    for k in range(j+1,width):
-                        dst[i,k] = mode
-                else:
-                    for k in range(j+2,j+uthreshold+1):
-                        if dst[i,k] == mode:
-                            break
-                    if dst[i,k]  == mode:
-                        for h in range(j+1,k):
-                            dst[i,h] = mode
-            #列消除
-            if dst[i,j] == mode and dst[i+1,j] == modeInver:
-
-                if i+vthreshold >= height:
-                    for k in range(i+1,height):
-                        dst[k,j] = mode
-                else:
-                    for k in range(i+2,i+vthreshold+1):
-                        if dst[k,j] == mode:
-                            break
-                    if dst[k,j] == mode:
-
-                        for h in range(i+1,k):
-                            dst[h,j] = mode
-    return dst
+from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
-    img = np.zeros((256,256),np.uint8)
-    img[50,0:255] = 255
-    img[0:255,100] = 255
+    img = cv2.imread("img/15.jpg",0)
+    edges = cv2.Canny(img,30,90)
     cv2.imshow("img",img)
-    img = delete_jut(img,5,5,1)
-    cv2.imshow("imgsrc",img)
-
+    cv2.imshow("edges",edges)
+    cv2.waitKey(0)
 
     cv2.waitKey(0)
